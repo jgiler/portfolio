8098120ff1f67aa71da91820f9e7234b66df8972
Jonathan Giler <jonathangiler@hotmail.comgit config --global user.name jonathan>
2019-10-14 00:50:36 -0400

    editing pics

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,22 @@
 import React from "react";
-import '../css/contact.css'
+import "../css/contact.css";
 
 const Contact = () => (
   <React.Fragment>
-    <div className='container'>
-      <div className='contacthero'>
-      <h1 className='contactme'>Contact Me!</h1>
+    <div className="container">
+      <div className="contacthero">
+        <h1 className="contactme">Contact Me!</h1>
       </div>
-      <p>
-        Please Feel Free to reach out to me in my social media links or my email!
-      </p>
-      <p className="email">jonathangiler@hotmail.com</p>
+      <div className="reachout">
+        <h2>Please Reach Out!</h2>
+        <p>
+          Please Feel Free to reach out to me in my social media links or my
+          email!
+        </p>
+        <p className="email">jonathangiler@hotmail.com</p>
+      </div>
+
+      <h3>Thank you for your time!</h3>
     </div>
   </React.Fragment>
 );
